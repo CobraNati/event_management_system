8b0b2c174806e4abd5b366e373fba566fc65193b
hawk234 <nahomtadesse83@gmail.com>
2026-06-14 22:39:10 +0300

    added database configurations and middleware for authentication

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database', err.stack);
+  } else {
+    console.log('Connected to the PostgreSQL database');
+  }
+});
+
+module.exports = pool;
